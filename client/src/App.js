42a59b51decef78b81a7f27c0956dc417f94312c
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-06 11:55:01 +0530

    added menu on home page

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from "./components/layout/Header";
 import Home from "./components/home/Home";
 import About from "./components/About";
+import Footer from "./components/layout/Footer";
 import "../src/style/app.scss"
 import "../src/style/header.scss"
 import "../src/style/home.scss"
 import "../src/style/founder.scss"
 import "../src/style/menu.scss"
+import "../src/style/footer.scss"
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
         </Routes>
+        <Footer/>
      </BrowserRouter>
   );
 }
