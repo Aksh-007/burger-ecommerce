6b5fc7f2343236da615d846bef91d49623167a59
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-07 06:14:24 +0530

    added confirm order page

--- client/src/components/cart/ConfirmOrder.jsx
+++ client/src/components/cart/ConfirmOrder.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const ConfirmOrder = () => {
   return (
-    <div>
-      confirm order
-    </div>
+    <section className="confirmOrder">
+        <main>
+            <h1>
+                Confirm Order
+            </h1>
+            <form>
+                <div>
+                    <label >Cash On Delivery</label>
+                    <input type='radio' name='payment' />
+                </div>
+                <div>
+                    <label >Upi</label>
+                    <input type='radio'name='payment'/>
+                </div>
+                <div>
+                    <label >Debit Card</label>
+                    <input type='radio' name='payment'/>
+                </div>
+
+                <button type='submit'>Place Order</button>
+            </form>
+        </main>
+    </section>
   )
 }
 
