e09fcd4af6399ce5f14c6129e36a17e76f79fa64
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-07 21:29:35 +0530

    fixes order details issue

--- client/src/components/order/OrderDetails.jsx
+++ client/src/components/order/OrderDetails.jsx
@@ -1,80 +1,107 @@
-import React from 'react'
+import React from "react";
 
 const OrderDetails = () => {
-    return (
-        <section className="orderDetails">
-          <main>
-            <h1>Order Details</h1>
+  return (
+    <section className="orderDetails">
+      <main>
+        <h1>Order Details</h1>
+        <div>
+          <h1>Shipping</h1>
+          <p>
+            <b>Address :</b>
+            {"sjda 12-32ss dsad"}
+          </p>
+        </div>
+        <div>
+          <h1>Contact</h1>
+          <p>
+            <b>Name :</b>
+            {"Akshay"}
+          </p>
+          <p>
+            <b>Phone :</b>
+            {2131232123}
+          </p>
+        </div>
+
+        <div>
+          <h1>Status</h1>
+          <p>
+            <b>Order Status :</b>
+            {"Processing"}
+          </p>
+          <p>
+            <b>Placed At :</b>
+            {"23-02-2002"}
+          </p>
+          <p>
+            <b>Delivered At :</b>
+            {"23-02-2003"}
+          </p>
+        </div>
+
+        <div>
+          <h1>Payment</h1>
+          <p>
+            <b>Payment Method :</b>
+            {"Online"}
+          </p>
+          <p>
+            <b>Payment Reference :</b> #{"sadsad"}
+          </p>
+          <p>
+            <b>Paid At :</b>
+            {"23-02-2003"}
+          </p>
+        </div>
+
+        <div>
+          <h1>Amount</h1>
+          <p>
+            <b>Items Total :</b> ₹{2132}
+          </p>
+          <p>
+            <b>Shipping Charges :</b> ₹{200}
+          </p>
+          <p>
+            <b>Tax :</b>₹{232}
+          </p>
+          <p>
+            <b>Total Amount :</b>₹ {232 + 200 + 2132}
+          </p>
+        </div>
+        <article>
+          <h1>Orderd items</h1>
+          <div>
+            <h4>Cheese Burger</h4>
             <div>
-              <h1>Shipping</h1>
-              <p>
-                <b>Address :</b>
-                 {"sjda 12-32ss dsad"}
-              </p>
+              <span>{52}</span> x <span>{252}</span>
             </div>
+          </div>
+          <div>
+            <h4>Chicken Burger</h4>
             <div>
-              <h1>Contact</h1>
-              <p>
-                <b>Name :</b>
-                 {"Akshay"}
-              </p>
-              <p>
-                <b>Phone :</b>
-                 {2131232123}
-              </p>
+              <span>{12}</span> x <span>{252}</span>
             </div>
-    
+          </div>
+          <div>
+            <h4>Mutton Burger</h4>
             <div>
-              <h1>Status</h1>
-              <p>
-                <b>Order Status :</b>
-                 {"Processing"}
-              </p>
-              <p>
-                <b>Placed At :</b>
-                 {"23-02-2002"}
-              </p>
-              <p>
-                <b>Delivered At :</b>
-                 {"23-02-2003"}
-              </p>
+              <span>{32}</span> x <span>{2252}</span>
             </div>
-    
+          </div>
+          <div>
+            <h4>
+              <strong>Sub Total</strong>
+            </h4>
             <div>
-              <h1>Payment</h1>
-              <p>
-                <b>Payment Method :</b>
-                 {"Online"}
-              </p>
-              <p>
-                <b>Payment Reference :</b> #{"sadsad"}
-              </p>
-              <p>
-                <b>Paid At :</b>
-                 {"23-02-2003"}
-              </p>
+              ₹{2132}
             </div>
-    
-            <div>
-              <h1>Amount</h1>
-              <p>
-                <b>Items Total :</b> ₹{2132}
-              </p>
-              <p>
-                <b>Shipping Charges :</b> ₹{200}
-              </p>
-              <p>
-                <b>Tax :</b>₹{232}
-              </p>
-              <p>
-                <b>Total Amount :</b>₹ {232 + 200 + 2132}
-              </p>
-            </div>
-    
-          
-          </main>
-        </section>
-      );
-    };
+          </div>
+        </article>
+      </main>
+    </section>
+  );
+};
 
 export default OrderDetails;
