2912ec69d1fd104aa1e16a7458229b8a48997235
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-13 12:06:42 +0530

    created api for get all stats for admin

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,6 +3,7 @@ import userModel  from "../models/user.schema.js";
 //importing asyncHandler
 import asyncHandler from "../services/asyncHandler.js";
 import customError from "../utils/customError.js";
+import orderModel from "../models/order.schema.js"
 
 
 // this will give you login user information as passport save info in req.user
@@ -45,4 +46,41 @@ export const getAllUsers = asyncHandler(async(req, res, next)=>{
         success:true,
         allUsers,
     })
+});
+
+
+
+//api to get stats to admin 
+export const getAdminStats = asyncHandler(async(req, res, next) =>{
+
+    const usersCount = await userModel.countDocuments();
+    
+    // here we want array of order to do classification 
+    const allOrders = await orderModel.find({});
+
+    // creating filter method for sorting differnt order 
+    const preparingOrder = allOrders.filter((e) => e.orderStatus === "Preparing");
+    const pickUpOrder = allOrders.filter((e) => e.orderStatus === "PickUp");
+    const deliveredOrder = allOrders.filter((e) => e.orderStatus === "Delivered");
+
+    let totalIncome = 0;
+
+    //add all amount using loop
+    allOrders.forEach( (e) =>{
+        totalIncome += e.totalAmout
+    });
+
+    //sending response
+    res.status(200).json({
+        success:true,
+        usersCount,
+        ordersCount:{
+            total : orderModel.length,
+            Preparing:preparingOrder.length,
+            PickUp : pickUpOrder.length,
+            Delivered : deliveredOrder.length,
+        },
+        totalIncome,
+    })
+
 })
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,39 +1,46 @@
 import express from "express";
 import passport from "passport";
-import { myProfile, logout, getAllUsers } from "../controllers/user.controller.js";
+import {
+    myProfile,
+    logout,
+    getAllUsers,
+    getAdminStats
+} from "../controllers/user.controller.js";
 import { isAuthenticated, authorisedAdmin } from "../middleware/authMiddleware.js";
 
-  const router = express.Router();
+const router = express.Router();
 
-    //here we are using passport to authenticate user using google auth
-    router.get("/googlelogin",
-        passport.authenticate("google", {
-            scope: ["profile"],
-        })
-    );
+//here we are using passport to authenticate user using google auth
+router.get("/googlelogin",
+    passport.authenticate("google", {
+        scope: ["profile"],
+    })
+);
 
-    // after authenticate from google redirect to this url{FRONTEND_URL}
-    // router.get("/login",
-    //     passport.authenticate('google',{
-    //         scope:["profile"],
-    //         successRedirect: process.env.FRONTEND_URL,
-    //     })
-    // )
-    
-    //temproparly setting it but we have to authenticate it 
-    router.get("/login", 
+// after authenticate from google redirect to this url{FRONTEND_URL}
+// router.get("/login",
+//     passport.authenticate('google',{
+//         scope:["profile"],
+//         successRedirect: process.env.FRONTEND_URL,
+//     })
+// )
+
+//temproparly setting it but we have to authenticate it 
+router.get("/login",
     //we have to authenticate it 
     passport.authenticate("google"),
-    (req, res, next) =>{
-        res.send("logged in ")  
+    (req, res, next) => {
+        res.send("logged in ")
     });
-    
-    // this routes gives you information of login user
-    router.get('/me',isAuthenticated, myProfile);
 
-    router.get('/logout', logout );
+// this routes gives you information of login user
+router.get('/me', isAuthenticated, myProfile);
 
-    //admin route
-    router.get("/admin/allusers",isAuthenticated, authorisedAdmin, getAllUsers);
+router.get('/logout', logout);
 
-    export default router;
+//admin route
+router.get("/admin/allusers", isAuthenticated, authorisedAdmin, getAllUsers);
+
+router.get("/admin/stats", isAuthenticated, authorisedAdmin, getAdminStats)
+
+export default router;
