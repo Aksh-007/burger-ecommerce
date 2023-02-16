e76c8f5c094ed2b2612dfb8c70fe4a828df237b0
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-16 23:02:12 +0530

    some fixes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -13,7 +13,8 @@ const router = express.Router();
 //here we are using passport to authenticate user using google auth
 router.get("/login",
     passport.authenticate("google", {
-        scope: ["profile"],
+        // scope: ["profile"],
+        successRedirect: process.env.FRONTEND_URL,
     })
 );
 
@@ -32,7 +33,7 @@ router.get("/googlelogin",
     passport.authenticate("google",{
                 // scope means what information we want from google
                  scope:["profile"],
-                 successRedirect: process.env.FRONTEND_URL,
+                 
              }),
     (req, res, next) => {
         res.send("logged in ")
