57fcb432fb1228559f2697688fa1e9910ca31e05
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-16 23:10:51 +0530

    issue

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,29 +10,33 @@ import { isAuthenticated, authorisedAdmin } from "../middleware/authMiddleware.j
 
 const router = express.Router();
 
+//here we are using passport to authenticate user using google auth
+router.get("/login",
+    passport.authenticate("google", {
+        scope: ["profile"],
+    })
+);
+
+// after authenticate from google redirect to this url{FRONTEND_URL}
+// router.get("/login",
+//     passport.authenticate('google',{
+//         scope:["profile"],
+//         successRedirect: process.env.FRONTEND_URL,
+//     })
+// )
+
+//temproparly setting it but we have to authenticate it 
 // after authenticate from google redirect to this url{FRONTEND_URL}
 router.get("/googlelogin",
     //we have to authenticate it 
     passport.authenticate("google",{
                 // scope means what information we want from google
                  scope:["profile"],
-                 
+                 successRedirect: process.env.FRONTEND_URL,
              }),
     (req, res, next) => {
         res.send("logged in ")
     });
-
-
-//here we are using passport to authenticate user using google auth
-router.get("/login",
-    passport.authenticate("google", {
-        // scope: ["profile"],
-        successRedirect: process.env.FRONTEND_URL,
-    })
-);
-
-
-
 
 // this routes gives you information of login user
 router.get('/me', isAuthenticated, myProfile);
