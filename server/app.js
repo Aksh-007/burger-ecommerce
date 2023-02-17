ca35cb1dd206684e50cc3bc1a9c296e49730656d
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-17 11:07:20 +0530

    cookies issue

--- server/app.js
+++ server/app.js
@@ -29,9 +29,13 @@ app.use(session({
     saveUninitialized: false,
 
     cookie:{
-        secure:process.env.NODE_ENV === "development" ? false : true,
-        httpOnly:process.env.NODE_ENV === "development" ? false : true,
-        sameSite:process.env.NODE_ENV === "development" ? false : "none",
+        // secure:process.env.NODE_ENV === "development" ? false : true,
+        // httpOnly:process.env.NODE_ENV === "development" ? false : true,
+        // sameSite:process.env.NODE_ENV === "development" ? false : "none",
+
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
     },
 }));
 
