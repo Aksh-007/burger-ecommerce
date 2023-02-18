429c0efc801905d1db156f3d25f413664ff60db5
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-18 17:40:04 +0530

    wraped app in Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     {/* wraping the app with provider and passing store */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
