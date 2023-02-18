1851f2fc23394b7870f49a3a49dc0a178e8653c4
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-18 17:37:54 +0530

    created store

---
+++ client/src/redux/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit"
+
+const store = configureStore({
+    reducer:{
+
+    },
+});
+
+
+export default store;
+
+export const server = "https://burger-ecommerce-8qcrilvut-aksh-007.vercel.app/api/v1";
